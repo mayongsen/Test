d1e5e9abdd0db83fa57e33158879acc5235acf2b
mayongsen <472365893@qq.com>
2019-10-14 18:04:49 +0800

    第一次提交

---
+++ com/xinzhi/pojo/Test.java
@@ -0,0 +1,22 @@
+package com.xinzhi.pojo;
+
+public class Test {
+    private String user;
+    private String password;
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
